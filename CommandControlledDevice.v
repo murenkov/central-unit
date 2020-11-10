fddaaf51885f0d0b8e170734a452925eff0e4b91
murenkov <yaroslav.murenkov@gmail.com>
2020-11-10 09:39:10 +0300

    Add command controlled device

---
+++ CommandControlledDevice.v
@@ -0,0 +1,58 @@
+/*
+* Command Controlled Device
+*/
+
+module CommandControlledDevice(
+    input clock, reset,
+    input [3:0] data_in,
+
+    output [3:0] data_out,
+    output [2:0] port_id,
+    output port_read, port_write
+);
+
+    wire [16:0] control_bus;
+    reg carry_flag, zero_flag;
+
+    RALU RALU(
+        .clock  (clock),
+        .reset  (reset),
+        .DataIn (data_in),
+        .S      (control_bus[16:13]),
+        .M      (control_bus[12]),
+        .Pin    (control_bus[11]),
+        .ISR    (control_bus[10]),
+        .ISL    (control_bus[9]),
+        .A      (control_bus[8]),
+        .wr     (control_bus[7]),
+        .adr    (control_bus[6:4]),
+        .v      (control_bus[3:0]),
+        .R      (data_out),
+        .Pout   (Pout)
+    );
+
+    ControlUnit ControlUnit(
+        .clock       (clock),
+        .reset       (reset),
+        .carry_flag  (carry_flag),
+        .zero_flag   (zero_flag),
+        .control_bus (control_bus)
+    );
+
+    assign port_read = control_bus[8] & control_bus[0];
+    assign port_write = control_bus[3];
+    assign port_id = control_bus[6:4];
+
+    always @(posedge clock or posedge reset) begin
+        if (!reset) begin
+            carry_flag <= 0;
+            zero_flag <= 0;
+        end
+        else begin
+            carry_flag <= Pout;
+            zero_flag <= ~(data_out[3] | data_out[2] | data_out[1] | data_out[0]);
+        end
+    end
+
+endmodule
+
---
+++ ControlUnit.v
@@ -0,0 +1,51 @@
+/*
+* Control Unit
+*/
+module ControlUnit(
+    input clock, reset,
+    input carry_flag, zero_flag,
+
+    output [16:0] control_bus
+);
+
+    reg [19:0] ROM [0:255];
+    initial $readmemb("commands.bin", ROM);
+    reg [6:0] program_counter = 0;
+    wire [19:0] command;
+
+    assign command = ROM[program_counter];
+    assign control_bus = command[16:0];
+
+    always @(posedge clock or posedge reset) begin
+        if (reset)
+            program_counter <= 0;
+        else case (command[19:17])
+            3'b000:
+                program_counter <= program_counter + 1;
+
+            3'b001:
+                program_counter <= (!zero_flag) ? command[6:0] : program_counter + 1;
+
+            3'b010:
+                program_counter <= (carry_flag && !zero_flag) ? command[6:0] : program_counter + 1;
+
+            3'b011:
+                program_counter <= (!carry_flag && !zero_flag) ? command[6:0] : program_counter + 1;
+
+            3'b100:
+                program_counter <= (zero_flag) ? command[6:0] : program_counter + 1;
+
+            3'b101:
+                program_counter <= (carry_flag && zero_flag) ? command[6:0] : program_counter + 1;
+
+            3'b110:
+                program_counter <= (!carry_flag && zero_flag) ? command[6:0] : program_counter + 1;
+
+            3'b111:
+                program_counter <= command[6:0];
+        endcase
+    end
+
+
+endmodule
+
